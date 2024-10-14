59325dc32439224ff4b0ef9869e0412c4a346a8b
mlobeto <mercedeslobeto@gmail.com>
2024-10-14 14:38:09 -0300

    logos listos

--- TorneoFront/src/components/Footer.jsx
+++ TorneoFront/src/components/Footer.jsx
@@ -1,30 +1,32 @@
 // src/components/Footer.jsx
 
-import logo1 from '../assets/logo1.png';
-import logo2 from '../assets/logo2.png';
-import logo3 from '../assets/logo3.png';
-import logo4 from '../assets/logo4.png';
-import logo5 from '../assets/logo5.png';
-import logo6 from '../assets/logo6.png';
+import logo1 from '../assets/institucionEducativa.png';
+ import logo2 from '../assets/AsoPadres.png';
+ import logo3 from '../assets/ALCALDIA.png';
+ import logo4 from '../assets/turismo.png';
+ import logo5 from '../assets/logoparafooter.png';
+ import logo6 from '../assets/code.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-white w-full py-6">
+    <footer className="bg-white w-3xl py-6 p-6">
       <div className="container mx-auto flex justify-center space-x-6">
         {/* Enlace 1 */}
+         <a href="https://link3.com" target="_blank" rel="noopener noreferrer">
+          <img src={logo3} alt="Logo 3" className="h-12 object-contain" />
+        </a>
+       
         <a href="https://link1.com" target="_blank" rel="noopener noreferrer">
           <img src={logo1} alt="Logo 1" className="h-12 object-contain" />
         </a>
 
         {/* Enlace 2 */}
-        <a href="https://link2.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://link2.com" target="_blank" rel="noopener noreferrer">
           <img src={logo2} alt="Logo 2" className="h-12 object-contain" />
-        </a>
+        </a> 
 
-        {/* Enlace 3 */}
-        <a href="https://link3.com" target="_blank" rel="noopener noreferrer">
-          <img src={logo3} alt="Logo 3" className="h-12 object-contain" />
-        </a>
+        
+      
 
         {/* Enlace 4 */}
         <a href="https://link4.com" target="_blank" rel="noopener noreferrer">
@@ -37,9 +39,9 @@ const Footer = () => {
         </a>
 
         {/* Enlace 6 */}
-        <a href="https://link6.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/code.desarrolladoresweb?igsh=cm5rbHI3bWluYzU2&utm_source=qr" target="_blank" rel="noopener noreferrer">
           <img src={logo6} alt="Logo 6" className="h-12 object-contain" />
-        </a>
+        </a> 
       </div>
     </footer>
   );
